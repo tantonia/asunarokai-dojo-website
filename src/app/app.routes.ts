c70ce0acff3cd6fee8dcac4cf59b9a502557e175
A. Tsatsouli <tantonia01@gmail.com>
2025-01-26 20:02:15 +0200

    chore(app): cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { InfoComponent } from './components/info/info.component';
 import { HistoryComponent } from './components/history/history.component';
-import { NewsComponent } from './components/news/news.component';
 import { LinksComponent } from './components/links/links.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
@@ -12,7 +11,6 @@ export const routes: Routes = [
   { path: "about", component: AboutComponent },
   { path: "info", component: InfoComponent },
   { path: "history", component: HistoryComponent },
-  //{ path: "news", component: NewsComponent },
   { path: "links", component: LinksComponent },
   { path: "", redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
